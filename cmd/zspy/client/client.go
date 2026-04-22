9d2d3e4dc6b98f02ba661f016414e9cf0e788d46
jesc7 <jesc@mail.ru>
2026-04-22 21:09:12 +0400

    --- Auto Git Commit ---

--- cmd/zspy/client/client.go
+++ cmd/zspy/client/client.go
@@ -3,8 +3,14 @@ package client
 import (
 	"context"
 	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 type Config struct {
@@ -28,4 +34,76 @@ func Start(ctx context.Context, service bool) error {
 	}
 
 	return e
+}
+
+func runWSClient(ctx context.Context, addr, jwtToken string) {
+	u := url.URL{Scheme: "ws", Host: addr, Path: "/ws"}
+	header := http.Header{"Authorization": []string{"Bearer " + jwtToken}}
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Завершение работы: контекст отменен")
+			return
+		default:
+			// Попытка подключения
+			log.Printf("Подключение к %s...", u.String())
+			conn, _, err := websocket.DefaultDialer.DialContext(ctx, u.String(), header)
+			if err != nil {
+				log.Printf("Ошибка: %v. Повтор через %v", err, 5*time.Second)
+
+				// Ждем перед реконнектом, но следим за контекстом
+				select {
+				case <-ctx.Done():
+					return
+				case <-time.After(5 * time.Second):
+					continue
+				}
+			}
+
+			handleConnection(ctx, conn)
+		}
+	}
+}
+
+func handleConnection(ctx context.Context, conn *websocket.Conn) {
+	defer conn.Close()
+	done := make(chan struct{})
+
+	// Горутина чтения
+	go func() {
+		defer close(done)
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Printf("Ошибка чтения: %v", err)
+				return
+			}
+			log.Printf("Сообщение: %s", msg)
+		}
+	}()
+
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			// Изящное закрытие (Close Handshake)
+			log.Println("Закрытие соединения по контексту...")
+			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("Ошибка при закрытии:", err)
+			}
+			return
+		case <-done:
+			log.Println("Соединение разорвано сервером")
+			return
+		case <-ticker.C:
+			if err := conn.WriteMessage(websocket.TextMessage, []byte("ping")); err != nil {
+				log.Printf("Ошибка записи: %v", err)
+				return
+			}
+		}
+	}
 }
--- cmd/zspy/go.mod
+++ cmd/zspy/go.mod
@@ -3,6 +3,7 @@ module github.com/jesc7/zombot/cmd/zspy
 go 1.25.0
 
 require (
+	github.com/gorilla/websocket v1.5.3
 	github.com/kardianos/service v1.2.4
 	golang.org/x/sys v0.43.0
 )
