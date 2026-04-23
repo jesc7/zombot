82530a5798e12bc2e3318439b8bd340d13055ff2
jesc7 <jesc@mail.ru>
2026-04-23 10:35:44 +0400

    --- Auto Git Commit ---

--- server/ws/ws.go
+++ server/ws/ws.go
@@ -2,8 +2,10 @@ package ws
 
 import (
 	"context"
+	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/jesc7/zombot/server/types"
 )
 
@@ -12,16 +14,39 @@ type Message struct {
 }
 
 type WS struct {
+	cfg types.Config
 	In  chan Message
 	Out chan Message
 }
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
 func NewWS(ctx context.Context, cfg types.Config) *WS {
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		WSHandler(ctx, w, r)
-	})
-	return &WS{}
+	return &WS{
+		cfg: cfg,
+		In:  make(chan Message),
+		Out: make(chan Message),
+	}
 }
 
 func (s *WS) Run(ctx context.Context) {
+	mux := http.NewServeMux()
+	// Оборачиваем обработчик, чтобы передать контекст
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		WSHandler(ctx, w, r)
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		WSHandler(ctx, w, r)
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
