996139396efd179dd2def858eafd63a3a32f4314
jesc7 <jesc@mail.ru>
2026-04-26 13:56:34 +0400

    --- Auto Git Commit ---

--- cmd/zspy/shared/types.go
+++ cmd/zspy/shared/types.go
@@ -3,10 +3,15 @@ package shared
 import "time"
 
 type MessageDuties struct {
-	Days   int    `json:"days"`
-	Name   string `json:"name"`
-	Duties []struct {
-		Date     time.Time `json:"date"`
-		Employee string    `json:"Employee"`
-	}
+	Q struct {
+		Days int    `json:"days"`
+		Name string `json:"name"`
+	} `json:"q"`
+	A struct {
+		Duties []struct {
+			DutyType int       `json:"duty_type"`
+			Date     time.Time `json:"date"`
+			Employee string    `json:"Employee"`
+		}
+	} `json:"a"`
 }
