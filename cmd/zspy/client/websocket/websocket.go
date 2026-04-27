d9e6185aabf50833ff02968adf476aeb2a9b3bd9
jesc7 <jesc@mail.ru>
2026-04-27 20:43:06 +0400

    --- Auto Git Commit ---

--- cmd/zspy/client/websocket/websocket.go
+++ cmd/zspy/client/websocket/websocket.go
@@ -2,30 +2,36 @@ package websocket
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 	"net/url"
 	"time"
 
 	"github.com/gorilla/websocket"
+	"github.com/jesc7/zombot/cmd/zspy/client/types"
+	"github.com/jesc7/zombot/cmd/zspy/shared"
 )
 
-type WebSocketClient struct{}
+type WebSocketClient struct {
+	host   url.URL
+	header http.Header
+}
 
-func NewClient() *WebSocketClient {
-	return &WebSocketClient{}
+func NewWebSocketClient(cfg types.Config) *WebSocketClient {
+	return &WebSocketClient{
+		host:   url.URL{Scheme: "ws", Host: cfg.Addr, Path: "/ws"},
+		header: http.Header{"Authorization": []string{"Bearer " + cfg.Token}},
+	}
 }
 
 func (ws *WebSocketClient) Run(ctx context.Context) {
-	u := url.URL{Scheme: "ws", Host: cfg.Addr, Path: "/ws"}
-	header := http.Header{"Authorization": []string{"Bearer " + cfg.Token}}
-
 	for {
 		select {
 		case <-ctx.Done():
 			return
 
 		default:
-			conn, _, e := websocket.DefaultDialer.DialContext(ctx, u.String(), header)
+			conn, _, e := websocket.DefaultDialer.DialContext(ctx, ws.host.String(), ws.header)
 			if e != nil {
 				select {
 				case <-ctx.Done():
@@ -36,6 +42,56 @@ func (ws *WebSocketClient) Run(ctx context.Context) {
 				}
 			}
 			handle(ctx, conn)
+		}
+	}
+}
+
+func handle(ctx context.Context, conn *websocket.Conn) {
+	defer conn.Close()
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		for {
+			msg, raw, e := read(conn)
+			if e != nil {
+				return
+			}
+
+			switch msg.Type {
+			case shared.MT_UNDEFINED:
+
+			case shared.MT_DUTY:
+				var d shared.MessageDuties
+				if e = json.Unmarshal(raw, &d); e != nil {
+					continue
+				}
+				//d.A = duties.Duty(db, nil, d.Q)
+				write(conn, d)
+
+			default:
+				_ = raw
+			}
+		}
+	}()
+
+	tPing := time.NewTicker(10 * time.Second)
+	defer tPing.Stop()
+
+	for {
+		select {
+		case <-ctx.Done(): //выход по контексту
+			conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			return
+
+		case <-done: //сервер закрыл соединение
+			return
+
+		case <-tPing.C: //пингуем соединение
+			if e := conn.WriteMessage(websocket.PingMessage, nil); e != nil {
+				return
+			}
 		}
 	}
 }
