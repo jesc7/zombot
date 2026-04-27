8254e838644f26e9707d0930881f5ab2f1c1b613
jesc7 <jesc@mail.ru>
2026-04-27 14:01:05 +0400

    --- Auto Git Commit ---

--- server/max/bot/commands.go
+++ server/max/bot/commands.go
@@ -1,5 +1,11 @@
 package bot
 
-const (
-	reDuty string = `(?i)^дежур[а-я]*(?:(?:\s+(?P<name>[а-я]+))?(?:\s+(?P<days>\d+))?)?$`
+import "regexp"
+
+var (
+	reDuty = regexp.MustCompile(`(?i)^дежур[а-я]*(?:(?:\s+(?P<name>[а-я]+))?(?:\s+(?P<days>\d+))?)?$`)
 )
+
+func isDutyCommand(value string) (bool, string, int) {
+	regexp.MustCompile()
+}
