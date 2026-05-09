d1de37ac18b462876f0ffd4f3ed6aaf0bd2085e9
jesc7 <jesc@mail.ru>
2026-05-09 15:03:07 +0400

    --- Auto Git Commit ---

--- cmd/zspy/client/jp/planner/contacts.go
+++ cmd/zspy/client/jp/planner/contacts.go
@@ -1,1 +1,17 @@
 package planner
+
+import "time"
+
+type MessengerType int
+
+const (
+	MT_MAX MessengerType = iota
+	MT_TELEGRAM
+)
+
+type Search struct {
+	Until  time.Time
+	MT     MessengerType
+	Sender string
+	Text   string
+}
