438e39d129ae432ffe663a5cd7074bc361013a1b
jesc7 <jesc@mail.ru>
2026-05-13 10:02:50 +0400

    --- Auto Git Commit ---

--- cmd/zspy/client/jp/planner/planner.go
+++ cmd/zspy/client/jp/planner/planner.go
@@ -25,17 +25,28 @@ func Absents(ctx context.Context, db *sql.DB) ([]shared.Absent, error) {
 	rows, e := db.QueryContext(ctx, `
 		select t1, t2, u, lower(trim(iif(t1 = 0, c2, iif((t1 = 6) or (t1 = 7), c1 || iif(char_length(c1) > 0 and char_length(c2) > 0, ' / ', '') || c2, c1)))) as c, g
 		from (
-			select coalesce(t.tabel_type, -1) as t1, coalesce(tc.id, -1) as t2, u.username as u, coalesce(tt.caption, '') as c1, coalesce(tc.caption, '') as c2, coalesce(p.gender, 0) as g
+			select coalesce(t.tabel_type, -1) as t1, 
+				coalesce(tc.id, -1) as t2, 
+				u.username as u, 
+				coalesce(tt.caption, '') as c1, 
+				coalesce(tc.caption, '') as c2, 
+				coalesce(p.gender, 0) as g
 			from tabel t
 			join sp$users u on u.id = t.user_id
 			join u$personal p on u.id = p.user_id
 			left join vw_tabel_type tt on t.tabel_type = tt.id
 			left join (
-				select user_id, max(id) as id from tabel_history where dt = current_date and comments_id <> 2 group by 1
+				select user_id, max(id) as id 
+				from tabel_history 
+				where dt = current_date 
+					and comments_id <> 2 group by 1
 			) h on u.id = h.user_id
 			left join tabel_history th on h.id = th.id
 			left join tabel_history_comments tc on th.comments_id = tc.id
-			where u.status = 0 and not u.id in (-1, 0, 8, 82) and t.dt = current_date and th.time_in is null
+			where u.status = 0 
+				and not u.id in (-1, 0, 8, 82) 
+				and t.dt = current_date 
+				and th.time_in is null
 		) a
 		order by u
 	`)
